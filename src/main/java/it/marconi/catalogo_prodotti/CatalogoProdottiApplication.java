1f6b0104e58b89ce4aee9a8c756b73130757db31
lisa2503 <lisa.giordani@marconirovereto.it>
2024-05-05 08:45:48 +0200

    creazione progetto

---
+++ src/main/java/it/marconi/catalogo_prodotti/CatalogoProdottiApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.catalogo_prodotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdottiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/catalogo_prodotti/CatalogoProdottiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.catalogo_prodotti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoProdottiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
